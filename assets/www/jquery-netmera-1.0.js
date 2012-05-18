dc3a80f74cd61e136b755b1a0a5a3bb34acd9330
Turan KONAN <turankonan@hotmail.com>
2012-05-18 17:38:55 +0300

    removed unused js

--- assets/www/jquery-netmera-1.0.js
+++ assets/www/jquery-netmera-1.0.js
@@ -1,11 +1,33 @@
+/**
+ * Copyright 2012 Inomera Research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Includes jQuery.js
+ * http://jquery.com
+ * Copyright 2011, John Resig
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://jquery.org/license
+ */
+
 function netmera() {
 	var _netmera = {};
 	var _st = null;
 	var _defaultParams = {};
 	var _exceptions = {};
+
 	var _request = {};
 	_request.params = {};
-	
 	_request.params.contentActionToken = "contentActionToken";
 	_request.params.path = "path";
 	_request.params.contentType = "contentType";
@@ -466,6 +488,9 @@ function netmera() {
 			
 			if (_searchText) {
 				_data[_request.params.searchText] = _searchText;
+				_data[_request.params.sortBy] = "";
+				_data[_request.params.sortOrder] = "";
+			} else {
 				_data[_request.params.sortBy] = _sortBy;
 				_data[_request.params.sortOrder] = _sortOrder;
 			}
